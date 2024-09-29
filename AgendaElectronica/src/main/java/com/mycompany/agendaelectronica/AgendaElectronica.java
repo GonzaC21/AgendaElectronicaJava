83211d4cc51f51a3aeb688cb4cb5da7a2848d750
Gonzalo <gonzalocallejas4@gmail.com>
2024-09-28 21:27:58 -0300

    update 1

---
+++ AgendaElectronica/src/main/java/com/mycompany/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agendaelectronica;
+
+import igu.Principal;
+
+
+
+
+
+
+/**
+ *
+ * @author gonza
+ */
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
